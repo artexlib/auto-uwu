b3e4903404d827131f3cabe568e0e68648f165bf
ketchdrown <artexlib@gmail.com>
2022-10-11 03:16:44 +0200

    v1.0

---
+++ README.md
@@ -0,0 +1,28 @@
+# auto-uwu
+- Auto response with UwU, OwO, QwQ... whenever hooked at a specified channel.
+- More UwUs futures soon.
+- Custom UI soon.
+- Report me any issues/bugs.
+- Check Commands.
+
+Note: All commands start with the prefix `/8 uwu`
+
+## Commands
+Command | Description
+--- | ---
+/8 uwu | Enables/Disabled module.
+/8 uwu ch (ID) | Specifies channel output && input. ID must be a number.
+/8 uwu help | In-game commands list
+
+## FAQ Channels
+ID | Channel | FAQ
+--- | --- | ---
+0 | Say | Catch UwU + response in Say
+1 | Party | Catch UwU + response in Party
+2 | Guild | Catch UwU + response in Guild
+3 | Area | Catch UwU + response in Area
+4 | Trade | Catch UwU + response in Trade
+27 | Global | Catch UwU + response in Global
+32 | Raid | Catch UwU + response in Raid
+
+![Alt Text](https://s4.gifyu.com/images/dcx3nhc-6718091c-11a0-4f78-a333-9e9eef5d2e8f.gif)
---
+++ index.js
@@ -0,0 +1,187 @@
+module.exports = function uwuCore(uwu) {
+
+    let isEnabled = uwu.settings.isEnabled;
+    let uChannel = uwu.settings.uChannel;
+    let randomMs = Math.floor(Math.random() * 402) + 807;
+    let used = ['uwu', 'owo', 'qwq', 'qvq', 'ewo', 'uwo', 'q_q', 'owu', 'owe', 'ewe'];
+
+    uwu.command.add('uwu', (...args) => {
+        switch(args[0]) {
+            case undefined:
+                isEnabled = !isEnabled;
+                msg('Mod is now ' + (isEnabled ? 'enabled' : 'disabled') + '.');
+                break
+            case 'ch': case 'channel':
+                if (!args[1]) {
+                    msg('Plase enter channel number.')
+                    return
+                }
+                if (args[1]) {
+                    if (args[1] === "0") {
+                        uChannel = parseInt(args[1])
+                        msg('Output channel set to: SAY')
+                        return
+                    }
+                    if (args[1] === "1") {
+                        uChannel = parseInt(args[1])
+                        msg('Output channel set to: PARTY')
+                        return
+                    }
+                    if (args[1] === "2") {
+                        uChannel = parseInt(args[1])
+                        msg('Output channel set to: GUILD')
+                        return
+                    }
+                    if (args[1] === "3") {
+                        uChannel = parseInt(args[1])
+                        msg('Output channel set to: AREA')
+                        return
+                    }
+                    if (args[1] === "4") {
+                        uChannel = parseInt(args[1])
+                        msg('Output channel set to: TRADE')
+                        return
+                    }
+                    if (args[1] === "27") {
+                        uChannel = parseInt(args[1])
+                        msg('Output channel set to: GLOBAL')
+                        return
+                    }
+                    if (args[1] === "32") {
+                        uChannel = parseInt(args[1])
+                        msg('Output channel set to: RAID')
+                        return
+                    }
+                } else {
+                    msg('Unknown channel! Please, check the FAQ/HELP before reporting a bug.')
+                    return
+                }
+            case 'help': case 'faq': case 'info':
+                msg('Commands:')
+                msg('/8 uwu - on/off.')
+                msg('/8 uwu ch - switch ch.')
+                msg('Have Fun, ' + uwu.game.me.name + '!')
+                return
+            default:
+                msg('Unknown command!')
+                return
+        }
+    });
+
+    // Functions
+
+    function msg(mSm) { 
+        uwu.command.message(mSm); 
+    }
+
+    /*function ChatSender(m, e) {
+        let rMs;
+        rMs = setTimeout(ChatSender, randomMs);
+        clearTimeout(rMs);
+        uwu.send('CHAT', 1, {
+            message: m,
+            channel: e
+        })
+    }*/
+
+    // Hooks
+
+    uwu.hook('S_CHAT', 3 , (event) => {
+        if (!isEnabled) return;
+        if (event.name === uwu.game.me.name) return;
+        if (event.message.toLowerCase().includes("uwu") && event.channel == uChannel) {
+            uwu.send('C_CHAT', 1, {
+                message: 'UwU',
+                channel: event.channel
+        })} 
+    });
+
+    uwu.hook('S_CHAT', 3 , (event) => {
+        if (!isEnabled) return;
+        if (event.name === uwu.game.me.name) return;
+        if (event.message.toLowerCase().includes("owo") && event.channel == uChannel) {
+            uwu.send('C_CHAT', 1, {
+                message: 'OwO',
+                channel: event.channel
+        })}
+    });
+
+    uwu.hook('S_CHAT', 3 , (event) => {
+        if (!isEnabled) return;
+        if (event.name === uwu.game.me.name) return;
+        if (event.message.toLowerCase().includes("qwq") && event.channel == uChannel) {
+            uwu.send('C_CHAT', 1, {
+                message: 'QwQ',
+                channel: event.channel
+        })}
+    });
+
+    uwu.hook('S_CHAT', 3 , (event) => {
+        if (!isEnabled) return;
+        if (event.name === uwu.game.me.name) return;
+        if (event.message.toLowerCase().includes("qvq") && event.channel == uChannel) {
+            uwu.send('C_CHAT', 1, {
+                message: 'QvQ',
+                channel: event.channel
+        })}
+    });
+
+    uwu.hook('S_CHAT', 3 , (event) => {
+        if (!isEnabled) return;
+        if (event.name === uwu.game.me.name) return;
+        if (event.message.toLowerCase().includes("ewo") && event.channel == uChannel) {
+            uwu.send('C_CHAT', 1, {
+                message: 'EwO',
+                channel: event.channel
+        })}
+    });
+    
+    uwu.hook('S_CHAT', 3 , (event) => {
+        if (!isEnabled) return;
+        if (event.name === uwu.game.me.name) return;
+        if (event.message.toLowerCase().includes("uwo") && event.channel == uChannel) {
+            uwu.send('C_CHAT', 1, {
+                message: 'UwO',
+                channel: event.channel
+        })}
+    });
+
+    uwu.hook('S_CHAT', 3 , (event) => {
+        if (!isEnabled) return;
+        if (event.name === uwu.game.me.name) return;
+        if (event.message.toLowerCase().includes("q_q") && event.channel == uChannel) {
+            uwu.send('C_CHAT', 1, {
+                message: 'Q_Q',
+                channel: event.channel
+        })}
+    });
+
+    uwu.hook('S_CHAT', 3 , (event) => {
+        if (!isEnabled) return;
+        if (event.name === uwu.game.me.name) return;
+        if (event.message.toLowerCase().includes("owu") && event.channel == uChannel) {
+            uwu.send('C_CHAT', 1, {
+                message: 'OwU',
+                channel: event.channel
+        })}
+    });
+
+    uwu.hook('S_CHAT', 3 , (event) => {
+        if (!isEnabled) return;
+        if (event.name === uwu.game.me.name) return;
+        if (event.message.toLowerCase().includes("OwE") && event.channel == uChannel) {
+            uwu.send('C_CHAT', 1, {
+                message: 'OwE',
+                channel: event.channel
+        })}
+    });
+
+    uwu.hook('S_CHAT', 3 , (event) => {
+        if (!isEnabled) return;
+        if (event.name === uwu.game.me.name) return;
+        if (event.message.toLowerCase().includes("ewe") && event.channel == uChannel) {
+            ChatSender("EwE", event.channel)    
+        }
+    });
+
+}
